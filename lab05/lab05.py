ad21eb9a8d8355706c8e6a518e9e632399c5dc54
Dal Basnet <dbb5698@psu.edu>
2026-02-22 13:11:25 -0500

    Lab 5 commit message:

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -13,7 +13,7 @@ def process_function_call(function_call):
 
     return globals()[name](**args)
 
-def roll_for(skill, dc, player):
+def roll_for(skill, dc, player="Adventurer"):
     n_dice = 1
     sides = 20
     roll = sum([random.randint(1, sides) for _ in range(n_dice)])
@@ -25,8 +25,12 @@ def roll_for(skill, dc, player):
 def process_response(self, response):
     # Fill out this function to process the response from the LLM
     # and make the function call
-    # Hint: check response.message.tool_calls and use process_function_call
-    return response
+    # Hint: check response.message.tool_calls and use process_function_call  
+        if response.message.tool_calls:
+            for tool_call in response.message.tool_calls:
+                tool_response = process_function_call(tool_call.function)
+                print(f'Tool response: {tool_response}')
+        return response
 
 run_console_chat(template_file='lab05/lab05_dice_template.json',
                  process_response=process_response)
